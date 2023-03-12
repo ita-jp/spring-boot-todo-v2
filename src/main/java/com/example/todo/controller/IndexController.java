2be932e0a6dead3bfdb6420284bc23f61284d08e
ita-jp <tn00gm@gmail.com>
2023-03-12 22:51:50 +0900

    複数のタスクを表示しよう

--- src/main/java/com/example/todo/controller/IndexController.java
+++ src/main/java/com/example/todo/controller/IndexController.java
@@ -5,13 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("task", new TaskDTO(1L,"Spring Boot を学ぶ", false));
+        var taskList = List.of(
+                new TaskDTO(1L,"Spring Boot を学ぶ", true),
+                new TaskDTO(2L,"Spring Security を学ぶ", false),
+                new TaskDTO(3L,"IntelliJ を学ぶ", false)
+        );
+        model.addAttribute("taskList", taskList);
         return "index";
     }
 }
