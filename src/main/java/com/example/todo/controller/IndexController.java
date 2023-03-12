a0c6808935d544f913de80d1a8b092a32cca9afa
ita-jp <tn00gm@gmail.com>
2023-03-12 23:28:05 +0900

    Lombok を使ってボイラプレートコードを減らそう

--- src/main/java/com/example/todo/controller/IndexController.java
+++ src/main/java/com/example/todo/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.todo.controller;
 
 import com.example.todo.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class IndexController {
 
     private final TaskService taskService;
-
-    public IndexController(TaskService taskService) {
-        this.taskService = taskService;
-    }
 
     @GetMapping
     public String index(Model model) {
