1ee2273fac1caa3f928a8335a1f8dbf05d224eb0
ita-jp <tn00gm@gmail.com>
2023-03-12 21:38:27 +0900

    トップページを表示してみよう

---
+++ src/main/java/com/example/todo/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    @ResponseBody
+    public String index() {
+        return "<h1>TODOs</h1>";
+    }
+}
