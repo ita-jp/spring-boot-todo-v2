d2a2a199e9d0d406751ea5c4ec2cf954f547e603
ita-jp <tn00gm@gmail.com>
2023-03-12 22:58:18 +0900

    record 型を使ってリファクタリングしよう

--- src/main/java/com/example/todo/controller/TaskDTO.java
+++ src/main/java/com/example/todo/controller/TaskDTO.java
@@ -1,26 +1,7 @@
 package com.example.todo.controller;
 
-public class TaskDTO {
-
-    private long id;
-    private String title;
-    private boolean isCompleted;
-
-    public TaskDTO(long id, String title, boolean isCompleted) {
-        this.id = id;
-        this.title = title;
-        this.isCompleted = isCompleted;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public boolean isCompleted() {
-        return isCompleted;
-    }
-}
+public record TaskDTO(
+        long id,
+        String title,
+        boolean isCompleted
+) {}
