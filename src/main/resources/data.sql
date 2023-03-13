498b1601565b63c6cfdbf8ac4feb5285633b9d33
ita-jp <tn00gm@gmail.com>
2023-03-13 09:30:54 +0900

    data.sql でデータを投入をしよう

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tasks (title, is_completed) VALUES ('タスク1', true);
+INSERT INTO tasks (title, is_completed) VALUES ('タスク2', false);
+INSERT INTO tasks (title, is_completed) VALUES ('タスク3', true);
